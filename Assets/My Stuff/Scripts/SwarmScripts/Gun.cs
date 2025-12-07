94bf182400530bdab4e7962f4f4b1b0537c7977b
mheftie296 <45861926+mheftie296@users.noreply.github.com>
2025-12-06 18:12:37 -0600

    Add enemy damage, exp gain, text popup

---
+++ Assets/My Stuff/Scripts/SwarmScripts/Gun.cs
@@ -0,0 +1,60 @@
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ZombieGun : MonoBehaviour
+{
+    public float range = 100f;
+
+    public ParticleSystem muzzleFlash;
+    private Camera fpsCamera;
+    private float nextTimeToFire;
+
+    void Start()
+    {
+        fpsCamera = GameObject.Find("MainCamera").GetComponent<Camera>();
+        nextTimeToFire = 0.0f;
+    }
+
+    void Update()
+    {
+        bool ready = Time.time >= nextTimeToFire;
+        if ( ready && Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        if (muzzleFlash != null)
+        {
+            muzzleFlash.Play();
+            Debug.Log("Muzzle flash played.");
+        }
+        Debug.Log("Shooting...");
+        RaycastHit hit;
+
+        if (Physics.Raycast(fpsCamera.transform.position, fpsCamera.transform.forward, out hit, range))
+        {
+            ZombieTarget target = hit.transform.GetComponent<ZombieTarget>();
+            if (target != null)
+            {
+                target.Process(hit);
+                if(target.dead)
+                {
+                    //gain experience for killing zombie
+                    PlayerScript player = GameObject.Find("PlayerCapsule").GetComponent<PlayerScript>();
+                    if (player != null){
+                        player.gainExperience(target.EXPAmount);
+                    }
+                    else
+                    {
+                        Debug.Log("PlayerScript component not found on Player.");
+                    }
+                }
+            }
+            nextTimeToFire = Time.time + 0.3f;
+        }
+    }
+}
---
+++ Assets/My Stuff/Scripts/SwarmScripts/ZombieTarget.cs
@@ -0,0 +1,102 @@
+using System;
+using TMPro;
+using UnityEditor.Callbacks;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ZombieTarget : MonoBehaviour
+{
+    public GameObject target;
+
+    public GameObject effectsManager;
+    public GameObject hitEffect;
+    public float effectDuration = 0.1f;
+    public AudioClip hitSound;
+    public float EXPAmount = 10.0f;
+    [SerializeField]
+    private TMP_Text textDisplay;
+
+    private EnemyController enemyController;
+
+    protected Effect effectScript;
+    float timer = 0.0f;
+
+    public bool dead = false;
+
+    private void Awake()
+    {
+        //effectScript = effectsManager.GetComponent<Effect>();
+        enemyController = GetComponent<EnemyController>(); 
+    }
+
+    private void Start()
+    {
+        textDisplay.text = "";
+        EXPAmount = enemyController.EXPAmount;
+    }
+
+    private void Update()
+    {
+        
+        if (textDisplay.text != "")
+        {
+            if (timer < 0.5f)
+            {
+                timer += Time.deltaTime;
+            }
+            else
+            {
+                textDisplay.text = "";
+                timer = 0.0f;
+            }
+        }
+        if (enemyController.health <= 0)
+        {
+            if(transform.position.y > -3.0f)
+            {
+                transform.position = new Vector3(transform.position.x, transform.position.y - Time.deltaTime, transform.position.z);
+            } else
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    public virtual void Process(RaycastHit hit)
+    {
+        //effectScript.Play(hit, hitSound, hitEffect, effectDuration);
+        Debug.Log("Hit registered on ZombieTarget.");
+        if (enemyController != null)
+        {   
+            float damage;
+            float dropoff = 0.5f; //lower means more damage up close, less at range
+            damage = (float)Math.Floor(500.0f * dropoff/(hit.distance + (50.0f * dropoff - 5.0f)));
+            //damage = 500; //temp for testing
+            enemyController.health -= damage;
+            textDisplay.text = ((int)damage).ToString();
+            textDisplay.transform.LookAt(Camera.main.transform);
+            //flip the text to face the camera correctly
+            textDisplay.transform.Rotate(0, 180, 0);
+            if (enemyController.health <= 0 && !dead)
+            {
+                Debug.Log("Zombie has died.");
+                dead = true;
+                Rigidbody rb = gameObject.AddComponent<Rigidbody>();
+                rb.isKinematic = true;
+                NavMeshAgent agent = GetComponent<NavMeshAgent>();
+                Animator animator = GetComponent<Animator>();
+                animator.enabled = false;
+                if (agent != null)
+                {
+                    //remove enemycontroller to stop errors from trying to access a removed component
+                    Destroy(enemyController);
+                    Destroy(agent);
+                } else
+                {
+                    Debug.Log("No NavMeshAgent found on " + gameObject.name);
+                }
+            }
+        }
+        //Destroy(target);
+    }
+}
